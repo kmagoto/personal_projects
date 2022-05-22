8c548d888d193964ea0b13b78f9c24fbad5b5465
kmagoto <kennedymagoto@gmail.com>
2022-05-22 10:39:39 +0300

    init

---
+++ madlibs/madlibs.py
@@ -0,0 +1,20 @@
+# Personal project
+# Madlib project
+# How to put string together- concatenation
+# 
+# A string that says "subscribe to _______"
+# youtuber = "Magoto" # empty string variables
+
+# print("subscribe to " + youtuber)
+# print(f"subscribe to {youtuber}")
+# print("subscribe to {}".format(youtuber))
+
+# adj = input("Adjective: ")
+# verb = input("verb: ")
+# verb2 = input("verb2: ")
+# famous_person = input("famous_person: ")
+
+# madlib = f"Computer programming is so {adj}: It makes me so excited all the time because I love to {verb}.\
+# Stay hydrated and {verb2} like you are {famous_person}!"
+
+# print(madlib)
